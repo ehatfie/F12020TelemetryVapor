418de4c7f771abd9afaa60309ab7fd70a141288d
Erik Hatfield <ehhatfield@protonmail.com>
2020-07-28 09:48:17 -0500

    adding game info

---
+++ Sources/App/Models/Generic Info/ButtonFlags.swift
@@ -0,0 +1,26 @@
+//
+//  ButtonFlags.swift
+//  
+//
+//  Created by Erik Hatfield on 7/28/20.
+//
+
+import Foundation
+
+enum ButtonFlags: UInt16 {
+    case CROSS = 0x0001
+    case TRIANGLE = 0x0002
+    case CIRCLE = 0x0004
+    case SQUARE = 0x0008
+    case D_PAD_LEFT = 0x0010
+    case D_PAD_RIGHT = 0x0020
+    case D_PAD_UP = 0x0040
+    case D_PAD_DOWN = 0x0080
+    case OPTIONS = 0x0100
+    case L1 = 0x0200
+    case R1 = 0x0400
+    case L2 = 0x0800
+    case R2 = 0x1000
+    case LEFT_STICK_CLICK = 0x2000
+    case RIGHT_STICK_CLICK = 0x4000
+}
---
+++ Sources/App/Models/Generic Info/PenaltyTypes.swift
@@ -0,0 +1,30 @@
+//
+//  PenaltyTypes.swift
+//  
+//
+//  Created by Erik Hatfield on 7/28/20.
+//
+
+import Foundation
+
+// enum?
+let penaltyTypes: [Int: String] = [
+    0: "Drive through",
+    1: "Stop Go",
+    2: "Grid penalty",
+    3: "Penalty reminder",
+    4: "Time penalty",
+    5: "Warning",
+    6: "Disqualified",
+    7: "Removed from formation lap",
+    8: "Parked too long timer",
+    9: "Tire regulations",
+    10: "This lap invalidated",
+    11: "This and next lap invalidated",
+    12: "This lap invalidated without reason",
+    13: "This and next lap invalidated without reason",
+    14: "This and previous lap invalidated",
+    15: "This and previous lap invalidated without reason",
+    16: "Retired",
+    17: "Black flag timer"
+]
