9f05791c9363c7d315a001c88bd9e4a961080ded
Erik Hatfield <ehhatfield@protonmail.com>
2021-06-13 19:59:53 -0500

    packet version bump

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0-rc"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
-        .package(url: "https://github.com/ehatfie/F12020TelemetryPackets.git", from: "0.0.15")
+        .package(url: "https://github.com/ehatfie/F12020TelemetryPackets.git", from: "0.0.16")
     ],
     targets: [
         .target(
